3f86af246888f95726f391472c45cd53eaf1188b
EMC <edwardcoad.ec@gmail.com>
2020-02-11 17:53:28 +1030

    making it heroku deployable

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,25 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Barista!993",
-    database: "eat_da_burger_db"
-});
+// var connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "Barista!993",
+//     database: "eat_da_burger_db"
+// });
+
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDBURL);
+}
+else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "Barista!993",
+        database: "eat_da_burger_db"
+    });
+};
 
 connection.connect(function(err) {
     if(err) {
